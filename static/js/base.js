7acf4bc4ac4577dfa8f745b9bffd4de565d0b42d
cage <517020375@qq.com>
2011-07-30 21:03:17 +0800

    js

--- static/js/base.js
+++ static/js/base.js
@@ -1,14 +1,18 @@
 $(document).ready(
 	function(){
 		var $t = $(".menu dt");
+		$("i.hide").eq(0).css("display","block");
 		$t.siblings("dd").eq(0).css("display","block");
 		$t.click(function(){
-			$t.siblings("dd").slideUp("speed");
 			if($(this).next("dd").css("display") == "none"){
 				$(this).next("dd").slideDown("speed");
+				$(this).find("i.show").css("display","none");
+				$(this).find("i.hide").css("display","block");
 			}
-			else{
+			else{ 
 				$(this).next("dd").slideUp("speed");
+				$(this).find("i.show").css("display","block");
+				$(this).find("i.hide").css("display","none");
 			}
 		});
 		var $m = $(".menu dd li");
